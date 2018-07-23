bf213de309c3e72fb7337ca8ef32ce7124242636
Eskild Ruud Mageli <eskild.emedd33@gmail.com>
2018-07-23 15:36:43 +0200

    Relocated HomeScreen to Home-folder.

--- Chemie_App/App.js
+++ Chemie_App/App.js
@@ -3,11 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Expo, Asset, AppLoading} from 'expo';
 import {createStackNavigator} from 'react-navigation';
 
-
+import HomeScreen from './src/Home/HomeScreen';
 import LoginScreen from './src/Login/LoginScreen';
 import NotificationScreen from './src/Notification/NotificationScreen';
 import SplashScreen from './src/Splash/SplashScreen';
-import HomeScreen from './src/HomeScreen';
 import SladderScreen from './src/Sladder/SladderScreen';
 import SettingsScreen from './src/Settings/SettingsScreen';
 import EventScreen from './src/Events/EventScreen';
@@ -20,6 +19,7 @@ export default class App extends React.Component {
   }
 }
 
+// TODO: Figure out if we need landscapemode, currently disabled
 // TODO: Fix splash from from preventing navigation from login to home
 // TODO: remove navigation from home to login or loging to splash with back arrow
 // TODO: Change style to be relative size to size of phone/tablet
---
+++ Chemie_App/src/Home/HomeScreen.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import {View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  BackHandler,
+} from 'react-native';
+
+export default class HomeScreen extends React.Component{
+  constructor(props){
+    super(props);
+
+  }
+  static navigationOptions = {
+    title: 'Home',
+    headerLeft: null,
+    headerStyle: {
+      backgroundColor: '#F9CF00'
+    },
+    headerTitle: (
+      <Image
+        source={require('./images/hclogo_headerIcon.png')}
+        style={{width:50, height:50, marginLeft:10,        }}
+      />
+  ),
+  };
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+  }
+  handleBackButton() {
+    return true;
+  }
+
+  render(){
+    return (
+        <View style={styles.container}>
+          <View style={styles.navigationContainer}>
+            <TouchableOpacity
+              style={styles.submitContainer}
+              onPress={()=>this.props.navigation.navigate('Sladder')}
+            >
+              <Image
+                resizeMode='contain'
+                style={styles.SladderLogo}
+                source={require('./images/Gossip_icon.png')}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.submitContainer}
+              onPress={()=>this.props.navigation.navigate('Notification')}
+            >
+              <Image
+                resizeMode='contain'
+                style={styles.SladderLogo}
+                source={require('./images/Coffee_icon.png')}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.submitContainer}
+              onPress={()=>this.props.navigation.navigate('Events')}
+            >
+              <Image
+                resizeMode='contain'
+                style={styles.SladderLogo}
+                source={require('./images/Party_icon.png')}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.submitContainer}
+              onPress={()=>this.props.navigation.navigate('Settings')}
+            ><Image
+              resizeMode='contain'
+              style={styles.SladderLogo}
+              source={require('./images/Settings_icon.png')}
+             />
+            </TouchableOpacity>
+
+
+          </View>
+          <View style={styles.newsContainer}>
+
+          </View>
+        </View>
+      );
+  }
+  }
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+    },
+    navigationContainer:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    headerImage:{
+
+    },
+    submitContainer:{
+      margin:10,
+      flex:1,
+      alignSelf: 'stretch',
+      paddingTop:20,
+      paddingBottom:20,
+      backgroundColor:'#F9CF00',
+
+      borderRadius:10,
+      borderWidth: 1,
+      borderColor: '#F9CF00',
+
+      alignItems:'center',
+      justifyContent:'center',
+
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.8,
+      shadowRadius: 3,
+      elevation: 2,
+    },
+    newsContainer:{
+      flex:3.5,
+      backgroundColor:"skyblue",
+    },
+    SladderLogo:{
+      width:70,
+      height:70,
+    }
+
+  });
