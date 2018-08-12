858958daeecc5e6f5cdaae9f4bde297ca63f1514
Eskild Ruud Mageli <eskild.emedd33@gmail.com>
2018-08-12 18:48:22 +0200

    Added Send notification method to website and delited copy of project

--- ChemieApp/src/Notification/NotificationScreen.js
+++ ChemieApp/src/Notification/NotificationScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import * as Progress from 'react-native-progress';
 
 import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
+  Switch,
+  Alert,
 } from 'react-native';
+
+import httpRequests from 'ChemieApp/src/Functions/HttpRequests'
+import base_params from 'ChemieApp/Params.js';
+const FETCH_PUSH_URL = base_params.base_url.concat("/api/pushnotification/submission/");
 
 export default class NotificationScreen extends React.Component{
   static navigationOptions = {
@@ -15,12 +23,96 @@ export default class NotificationScreen extends React.Component{
     };
   constructor(props){
     super(props);
+    this.state={
+      loading:false,
+      sendCoffee:false,
+      sendVaffler: false,
+      sendGoodies: false,
+      profile:props.navigation.state.params.profile,
+      authToken:props.navigation.state.params.authToken,
+    }
+    this.sendNotificationHttpRequest = this.sendNotificationHttpRequest.bind(this);
+  }
+  sendNotificationHttpRequest = async() =>{
+      if (!this.state.sendCoffee && !this.state.sendVaffler && !this.state.sendGoodies){
+        Alert.alert("Ups", "Du må minst velge et alternativ")
+      } else {
+        this.setState({loading:true})
+        let body = {
+          coffee:this.state.sendCoffee,
+          vaffler:this.state.sendVaffler,
+          goodies:this.state.sendGoodies,
+        }
+        console.log(FETCH_PUSH_URL);
+        let jsonResponse = await httpRequests.PostRequest(FETCH_PUSH_URL, body, this.state.authToken)
+        if (jsonResponse.httpStatus >= 200 && jsonResponse.httpStatus < 300){
+          Alert.alert("suksess", "Så snill du er som sier ifra til andre");
+        }
+        this.setState({loading:false})
+      }
   }
 render(){
+  if(this.state.loading){
+    // TODO: This needs to be chacked to IOS, https://github.com/oblador/react-native-progress
+    return(
+      <View style={styles.loadingContainer}>
+        <Progress.Circle size={80} indeterminate={true} color="black" />
+      </View>
+    );
+  }
   return(
     <View style={styles.container}>
 
-      <Text>NotificationScreen</Text>
+      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        <Text style={{textAlign:'center', fontSize:15}}>
+          Her kan du seende push-vasler til andre om kaffe/vaffler/godsaker på kontoret.
+        </Text>
+      </View>
+      <View style={{flex:2}}>
+        <View style={styles.switchContainer}>
+          <Switch
+            value={this.state.sendCoffee}
+            onValueChange={()=>{this.setState({
+              sendCoffee:!this.state.sendCoffee
+            })}}
+          />
+          <Text style={{fontWeight:'bold'}}>
+            Kaffe
+          </Text>
+        </View>
+        <View style={styles.switchContainer}>
+          <Switch
+            value={this.state.sendVaffler}
+            onValueChange={()=>{this.setState({
+              sendVaffler:!this.state.sendVaffler
+            })}}
+          />
+          <Text style={{fontWeight:'bold'}}>
+            Vaffler
+          </Text>
+        </View>
+        <View style={styles.switchContainer}>
+          <Switch
+            value={this.state.sendGoodies}
+            onValueChange={()=>{this.setState({
+              sendGoodies:!this.state.sendGoodies
+            })}}
+          />
+          <Text style={{fontWeight:'bold'}}>
+            Andre godsaker
+          </Text>
+        </View>
+      </View>
+      <View style={{flex:1}}>
+        <TouchableOpacity
+          style={styles.submitContainer}
+          onPress={this.sendNotificationHttpRequest}
+        >
+          <Text style={styles.submitText}>
+            Send push notification
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
     );
   }
@@ -30,5 +122,35 @@ const styles = StyleSheet.create({
   container:{
     flex:1
   },
+  switchContainer:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    marginLeft:50,
+    marginRight:50,
+  },
+  submitContainer:{
+    marginRight:20,
+    marginLeft:20,
+    marginTop:10,
+    alignSelf: 'stretch',
+    backgroundColor:'#F9CF00',
+    borderRadius:10,
+    height:50,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#F9CF00',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+    elevation: 2,
+  },
+  loadingContainer:{
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:50,
+  }
 
 });
--- ChemieApp_1/Params.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  base_url: 'http://192.168.0.17:8000',
-  social_event_url: 'https://chemie.no/events/social/',
-  bedpres_event_url: 'https://chemie.no/events/bedpres/',
-}
